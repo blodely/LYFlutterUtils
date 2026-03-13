f6a8e35d6d50d2f0772ab00abb3ec882f5309624
Rick Luo <luoyu@luoyu.space>
2026-03-14 02:22:59 +0800

    [Modify] theme brightness status check

--- lib/ly_flutter_utils.dart
+++ lib/ly_flutter_utils.dart
@@ -1,3 +1,17 @@
 library ly_flutter_utils;
 
+import 'package:flutter/material.dart';
+
 export 'debug_border.dart';
+
+class LYFlutterUtils {
+  // Singleton
+  static final LYFlutterUtils _kit = LYFlutterUtils._internal();
+  static LYFlutterUtils get kit => _kit;
+  LYFlutterUtils._internal();
+
+  // Theme brightness status
+  bool isThemeLight(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.light;
+  }
+}
