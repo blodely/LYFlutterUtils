0e61d2bcd2631165aec6388a79f1d660361bfb81
Rick Luo <luoyu@luoyu.space>
2026-03-22 17:32:27 +0800

    [Fix] url

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 # Script: publish.sh
 # Created by Luo Yu(indie.luo@gmail.com) at Thursday, June 16, 2022
 
-PUB_HOSTED_URL=http://pub.dev flutter pub publish
+PUB_HOSTED_URL=https://pub.dev flutter pub publish
