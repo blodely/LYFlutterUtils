f41711fa5f520bd57ddf7721a8edef4563f55809
Rick Luo <luoyu@luoyu.space>
2026-04-22 01:04:27 +0800

    [Fix] layout button

--- lib/ly_res_apple.dart
+++ lib/ly_res_apple.dart
@@ -2,19 +2,23 @@ import 'package:flutter/material.dart';
 
 class LYResApple extends StatelessWidget {
   final Widget? child;
-  const LYResApple({super.key, this.child});
+  final VoidCallback? ontap;
+  const LYResApple({super.key, this.child, this.ontap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 109,
-      height: 40,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        border: Border.all(color: Color(0xffa6a6a6), width: 1),
-        borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        width: 109,
+        height: 40,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          border: Border.all(color: Color(0xffa6a6a6), width: 1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: child,
       ),
-      child: child,
     );
   }
 
@@ -34,30 +38,36 @@ class LYResApple extends StatelessWidget {
       fontSize: 8,
       fontWeight: FontWeight.bold,
       fontFamily: 'SF Compact',
-      letterSpacing: 0.1,
+      letterSpacing: -0.3,
     );
   }
 }
 
 class LYResDlAppStore extends StatelessWidget {
-  final Widget? child;
-  const LYResDlAppStore({super.key, this.child});
+  final VoidCallback? ontap;
+  const LYResDlAppStore({super.key, this.ontap});
 
   @override
   Widget build(BuildContext context) {
     return LYResApple(
-      child: Row(
+      ontap: ontap,
+      child: Stack(
         children: [
-          SizedBox(width: 4),
-          Icon(Icons.apple, color: Colors.white, size: 26),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: -3,
-            children: [
-              Text('Download on the', style: LYResApple.subtitleStyle()),
-              Text('App Store', style: LYResApple.titleStyle()),
-            ],
+          Positioned(
+            top: 1,
+            bottom: 1,
+            left: 4,
+            child: Icon(Icons.apple, color: Colors.white, size: 26),
+          ),
+          Positioned(
+            top: 5,
+            left: 30,
+            child: Text('Download on the', style: LYResApple.subtitleStyle()),
+          ),
+          Positioned(
+            bottom: 5,
+            left: 30,
+            child: Text('App Store', style: LYResApple.titleStyle()),
           ),
         ],
       ),
